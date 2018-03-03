e91c50e8be7e6881d0dced1e25e5df15e061ed40
EmilienMingard <emilien.mingard@epfl.ch>
2018-03-03 15:15:35 +0100

    Stability Vs Impulsion

--- Snippets/Basic_Stability.m
+++ Snippets/Basic_Stability.m
@@ -11,10 +11,10 @@ Rocket = rocketReader('Rocket_Definition.txt');
 %--------------------------------------------------------------------------
 % 1 Equation: Id2a/dt2+C2da/dt+C1a = f1(t) without Roll (Decoupled Equ.)
 %--------------------------------------------------------------------------
-Amplitude = [];
-t = linspace(0,30,1000);
-V = [5:1:100];
-V_inf = 5;
+Amplitude1 = [];
+t1 = linspace(0,30,1000);
+V = [5:1:40];   % Rocket speed at rail tip
+V_inf = 5;      % Wind speed
 
 for Velocity = V
     
@@ -51,18 +51,57 @@ elseif damp <= 0
 end
 
 % 1.3 Max of Amplitude
-sol = a(t);
-Amplitude = [Amplitude max([max(sol) -min(sol)])];
+sol = a(t1);
+Amplitude1 = [Amplitude1 max([max(sol-Ms/C1) -min(sol-Ms/C1)])];
 figure (1);
-plot(t,sol*180/pi,'r');grid on;
+plot(t1,sol*180/pi,'r');grid on;
 end
 
 % 1.4 Results:
 figure(2);
-plot(V,Amplitude/pi*180,'r');hold on;grid on;
+plot(V,Amplitude1/pi*180,'r');hold on;grid on;
 xlabel('Velocity [m/s]');ylabel('Amplitude of oscillation [deg]');
 title('Amplitude vs Velocity at rail exit')
 
 %--------------------------------------------------------------------------
 % 2 Equation: Id2a/dt2+C2da/dt+C1a = delta(0) without Roll (Decoupled Equ.)
 %--------------------------------------------------------------------------
+Amplitude2 = [];
+t2 = linspace(0,30,1000);
+Impulsion = [1:1:10]; % Impulsion intensity
+Velocity = 25;      % Rocket speed at rail tip
+
+for H = Impulsion
+ 
+[Calpha, CP] = barrowmanLift(Rocket,0,Velocity/346,0); % No roll
+[CNa, Xp] = normalLift(Rocket,0,1.1,Velocity/346,0,0);
+C1 = CorrectionMoment(Rocket,CNa,Xp,Velocity);
+C2 = DampingMoment(Rocket,Calpha,CP,Velocity);
+
+% 1.2 Solutions:
+damp = C1/Rocket.rocket_I - C2^2/4/Rocket.rocket_I^2; % Damping ratio
+if damp > 0
+    D = C2/Rocket.rocket_I/2;
+    w = sqrt(damp);
+    a = @(t) H/Rocket.rocket_I/w*exp(-D*t).*sin(w*t);
+elseif damp == 0
+    D = C2/Rocket.rocket_I/2;
+    a = @(t) H/Rocket.rocket_I*t.*exp(-D*t);
+elseif damp <= 0
+    tau1 = 1/(C2/2/Rocket.rocket_I-sqrt(-damp));
+    tau2 = 1/(C2/2/Rocket.rocket_I+sqrt(-damp));
+    a = @(t) H*tau1*tau2/(tau1-tau2)/I_l*(exp(-t/tau1)-exp(-t/tau2));
+end
+
+% 1.3 Max of Amplitude
+sol = a(t2);
+Amplitude2 = [Amplitude2 max([max(sol) -min(sol)])];
+figure(3);
+plot(t2,sol*180/pi,'r');grid on;
+end
+
+% 1.4 Results:
+figure(4);
+plot(Impulsion,Amplitude2/pi*180,'r');hold on;grid on;
+xlabel('Velocity [m/s]');ylabel('Amplitude of oscillation [deg]');
+title('Amplitude vs Wind impulsion at rail exit')
