b4b6e2b4b4f43cab9a3581e8dd21738ad7a65a44
Malo Goury du Roslan <malo.goury@gmail.com>
2018-11-27 21:22:48 +0100

    update

--- Simulator_1D/Sim_1D_R3.m
+++ Simulator_1D/Sim_1D_R3.m
@@ -1,4 +1,4 @@
-function [T, X, T1, X1, T2, X2] = Sim_1D_R02(Rocket, Env, tspan, x0, AB_drag, theta, EventType, EventVal, EventDir)
+function [T, X, T1, X1, T2, X2] = Sim_1D_R3(Rocket, Env, tspan, x0, AB_drag, theta, thrust_err, ab_err, EventType, EventVal, EventDir)
 
 Option = odeset('RelTol', 1e-3, 'AbsTol', 1e-6);
 
@@ -15,23 +15,23 @@ switch(EventType)
         "'Alitude', 'Velocity' or 'None'."]);
 end
 
-[T, X] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 1.1, 1),...
+[T, X] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 1+thrust_err, 1+ab_err),...
                tspan, x0, Option);
-[T1, X1] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 1, 1),...
+[T1, X1] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 1, 1+ab_err),...
                tspan, x0, Option);
-[T2, X2] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 0.9, 1),...
+[T2, X2] = ode45(@(t,x) Rocket_Kinematic_R3(t, x, Rocket, Env, AB_drag, theta, 1-thrust_err, 1+ab_err),...
                tspan, x0, Option);
 
-    function [value, isterminal, direction] = AltEvent(T,X)
+    function [value, isterminal, direction] = AltEvent(T1,X1)
         %   Stop simulation at apogee
-        value = (X(1)< EventVal)-0.5;   % altitude reaches beginning of burn phase
+        value = (X1(1)< EventVal)-0.5;   % altitude reaches beginning of burn phase
         isterminal = 1; % Stop the integration
         direction = EventDir;      
     end
 
-    function [value, isterminal, direction] = SpeedEvent(T,X)
+    function [value, isterminal, direction] = SpeedEvent(T1,X1)
         %   Stop simulation at apogee
-        value = X(2)-EventVal;   % altitude reaches beginning of burn phase
+        value = X1(2)-EventVal;   % altitude reaches beginning of burn phase
         isterminal = 1; % Stop the integration
         direction = EventDir;      
     end
